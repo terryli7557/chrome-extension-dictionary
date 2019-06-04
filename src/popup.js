d736d063c1639af9857989454b8111fd2e65f5f1
terry <terryli7557@gmail.com>
2019-06-04 15:04:32 +1000

    Add a form

--- src/popup.js
+++ src/popup.js
@@ -1,3 +1,11 @@
+function openWinow(keyword){
+  window.open(
+    "https://dictionary.cambridge.org/dictionary/english-chinese-simplified/" +
+    keyword
+  );
+}
+
+
 chrome.tabs.query(
   { active: true, windowId: chrome.windows.WINDOW_ID_CURRENT },
   function(tab) {
@@ -5,11 +13,22 @@ chrome.tabs.query(
       response
     ) {
       if (response && response.data) {
-        window.open(
-          "https://dictionary.cambridge.org/dictionary/english-chinese-simplified/" +
-            response.data
-        );
+        openWinow(response.data)
       }
     });
   }
 );
+
+
+window.addEventListener('DOMContentLoaded', () => {
+
+  document.querySelector('form').addEventListener('submit',function(e){
+    e.preventDefault();
+    var v = document.querySelector('#dictionary_q').value;
+    if(v){
+      openWinow(v)
+    }
+  })
+});
+
+
